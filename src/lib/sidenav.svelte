d0a7bf46e2e2fd8128cc10121c5c004d953ef02c
DyllasDek <danchik.korneenko@yandex.ru>
2023-04-04 16:48:12 +0300

    add simple site with auth

---
+++ src/lib/sidenav.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+  import { page } from '$app/stores';
+  
+</script>
+<div>
+    <header>
+        <h1>Academic Assignment Management</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="{base}/">Dashboard</a></li>
+            <li><a href="{base}/assignments">Assignments</a></li>
+            <li><a href="{base}/courses">Courses</a></li>
+            <li><a href="{base}/grades">Grades</a></li>
+            <li><a href="{base}/settings">Settings</a></li>
+            {#if !$page.data.user}
+            <li><a href="{base}/login">Login</a></li>
+            <li><a href="{base}/register">Register</a></li>
+            {:else}
+            <li>
+              <form action="/logout" method="POST">
+                <button type="submit">Log out</button>
+              </form>
+
+            </li>
+            {/if}
+        </ul>
+    </nav>
+</div>
+
+
+<style>
+    /* Header */
+  header {
+    background-color: #333;
+    color: #fff;
+    padding: 10px;
+  }
+  
+  header h1 {
+    margin: 0;
+  }
+  
+  /* Navigation */
+  nav {
+    background-color: #f2f2f2;
+    padding: 10px;
+    text-align: center;
+    
+  }
+  
+  nav ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+  
+  nav li {
+    display: inline-block;
+    margin-right: 20px;
+  }
+  
+  nav a {
+    color: #333;
+    text-decoration: none;
+  }
+  
+  nav a:hover {
+    text-decoration: underline;
+  }
+  
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import './styles.css';
+    import SideNav from '$lib/sidenav.svelte';    
+  </script>
+
+
+<SideNav />
+
+<div>
+<slot />
+</div>
+
+<style>
+
+
+</style>
